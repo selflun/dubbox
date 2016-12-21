dc07ae151a460616fe59b2b081164df27fe7f054
qianjc <qianjc@unionpaysmart.com>
2016-12-21 16:19:06 +0800

    span annotation 基本框架实现
    Signed-off-by: qianjc <qianjc@unionpaysmart.com>

--- dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/trace/RpcTraceFilter.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/trace/RpcTraceFilter.java
@@ -8,6 +8,7 @@ import com.alibaba.dubbo.rpc.filter.trace.dapper.AttachmentUtil;
 import com.alibaba.dubbo.rpc.filter.trace.dapper.Constants;
 import com.alibaba.dubbo.rpc.filter.trace.dapper.EndPoint;
 import com.alibaba.dubbo.rpc.filter.trace.dapper.Span;
+import com.alibaba.dubbo.remoting.TimeoutException;
 
 /**
  * Copyright (C), 2016, 银联智惠信息服务（上海）有限公司
@@ -36,40 +37,71 @@ public class RpcTraceFilter implements Filter {
         EndPoint endPoint = tracer.buildEndPoint(context.getLocalAddressString(), context.getLocalPort(), "");
 
         Span span = null;
-        if (isConsumerSide) {
-            // 如果是消费者
-            LOGGER.info("消费者");
-            Span parentSpan = tracer.getParentSpan();
-            if (null == parentSpan) {
-                // 如果parentSpan为null, 表示该Span为root span
+        try {
+            if (isConsumerSide) {
+                // 如果是消费者
+                LOGGER.info("消费者");
+                Span parentSpan = tracer.getParentSpan();
+                if (null == parentSpan) {
+                    // 如果parentSpan为null, 表示该Span为root span
+                    // TODO: 设计serviceId
+                    span = tracer.newSpan(methodName, "");
+                } else {
+                    // 叶子span
+                    // TODO: 设计serviceId
+                    span = tracer.buildSpan(parentSpan.getTraceId(), parentSpan.getId(), tracer.generateSpanId(), methodName, parentSpan.isSample(), "");
+                }
+            } else if (isProviderSide) {
+                // 如果是生产者
+                LOGGER.info("生产者");
+                Long traceId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.TRACE_ID);
+                Long parentId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.PARENT_ID);
+                Long spanId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.SPAN_ID);
+                boolean isSample = traceId != null;
                 // TODO: 设计serviceId
-                span = tracer.newSpan(methodName, "");
-            } else {
-                // 叶子span
-                // TODO: 设计serviceId
-                span = tracer.buildSpan(parentSpan.getTraceId(), parentSpan.getId(), tracer.generateSpanId(), methodName, parentSpan.isSample(), "");
+                span = tracer.buildSpan(traceId, parentId, spanId, methodName, isSample, "");
             }
-        } else if (isProviderSide) {
-            // 如果是生产者
-            LOGGER.info("生产者");
-            Long traceId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.TRACE_ID);
-            Long parentId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.PARENT_ID);
-            Long spanId = AttachmentUtil.getAttachmentLong(rpcInvocation, Constants.SPAN_ID);
-            boolean isSample = traceId != null;
-            // TODO: 设计serviceId
-            span = tracer.buildSpan(traceId, parentId, spanId, methodName, isSample, "");
+
+            LOGGER.info("我是Span" + span.toString());
+
+            // 调用具体业务逻辑之前处理
+            this.invokeBefore(span, isConsumerSide, isProviderSide);
+            // 传递附件到RpcInvocation, 传递到下游，确保下游能够收到traceId等相关信息，保证请求能够标记到正确的traceId
+            this.setAttachment(rpcInvocation, span);
+            // 执行具体的业务或者下游的filter
+            Result result = invoker.invoke(invocation);
+
+            if (result.hasException()) {
+                // 如果在请求过程中发生了异常, 需要进行异常的处理和相关annotation的记录
+                this.processException();
+            }
+
+            return result;
+        } catch (RpcException e) {
+            if (null != e.getCause() && e.getCause() instanceof TimeoutException) {
+                // 如果异常是com.alibaba.dubbo.remoting.TimeoutException
+                this.processTimeoutException();
+            } else {
+                // 其他异常
+                this.processException();
+            }
+            // 将异常抛出去
+            throw e;
+        } finally {
+            // 调用具体业务逻辑之后处理
+            if (null != span) {
+                this.invokeAfter(span, isConsumerSide, isProviderSide);
+            }
         }
 
-        LOGGER.info("我是Span" + span.toString());
+    }
 
-        this.invokeBefore(span, isConsumerSide, isProviderSide);
+    private void processTimeoutException() {
 
-        this.setAttachment(rpcInvocation, span);
+    }
 
-        Result result = invoker.invoke(invocation);
+    private void processException() {
 
-        this.invokeAfter(span, isConsumerSide, isProviderSide);
-        return result;
     }
 
     /**
@@ -96,7 +128,15 @@ public class RpcTraceFilter implements Filter {
      */
     private void invokeBefore(Span span, boolean isConsumerSide, boolean isProviderSide) {
         Tracer tracer = Tracer.getInstance();
-        if (isProviderSide) {
+        if (isConsumerSide && span.isSample()) {
+            // 如果是消费者, ClientSend
+
+        } else if (isProviderSide) {
+            // 如果是提供者
+            if (span.isSample()) {
+                // ServerReceive
+            }
+            // 将该span作为parentSpan设置到ThreadLocal中
             tracer.setParentSpan(span);
         }
     }
@@ -109,7 +149,13 @@ public class RpcTraceFilter implements Filter {
      */
     private void invokeAfter(Span span, boolean isConsumerSide, boolean isProviderSide) {
         Tracer tracer = Tracer.getInstance();
-        if (isProviderSide) {
+        if (isConsumerSide && span.isSample()) {
+            // 如果是消费者, ClientReceive
+        } else if (isProviderSide) {
+            // 如果是提供者
+            if (span.isSample()) {
+                // ServerSend
+            }
             tracer.removeParentSpan();
         }
     }
---
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/com/alibaba/dubbo/rpc/filter/trace/dapper/ExceptionType.java
@@ -0,0 +1,12 @@
+package com.alibaba.dubbo.rpc.filter.trace.dapper;
+
+/**
+ * Copyright (C), 2016, 银联智惠信息服务（上海）有限公司
+ *
+ * @author qianjc
+ * @version 0.0.1
+ * @desc
+ * @date 2016-12-21 15:13:28
+ */
+public enum ExceptionType {
+}
